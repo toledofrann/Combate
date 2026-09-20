c04880565bebc42f88e8a9f73e4d50db06bc1c6f
toledofrann <toledofrancisco0303@gmail.com>
2026-09-20 16:50:30 -0300

    test: verifica que el soldado muere al recibir un disparo (fase roja)

--- test_tropas.py
+++ test_tropas.py
@@ -7,6 +7,11 @@ class TestSoldado(unittest.TestCase):
         soldado = Soldado()
         self.assertTrue(soldado.esta_vivo())
 
+    def test_soldado_muere_al_recibir_disparo(self):
+        soldado = Soldado()
+        soldado.recibir_disparo()
+        self.assertFalse(soldado.esta_vivo())
+
 
 if __name__ == '__main__':
     unittest.main()
