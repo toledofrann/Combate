8fe0f01171ec7dd8426086e2cee3ed38045dd608
toledofrann <toledofrancisco0303@gmail.com>
2026-09-20 17:14:40 -0300

    refactor: extrae logica comun a la clase abstracta Tropa

--- tropas.py
+++ tropas.py
@@ -1,9 +1,15 @@
-class Soldado:
-    def __init__(self, vida = 1):
-        self.vida = vida
+from abc import ABC, abstractmethod
 
+class Tropa(ABC):
+    def __init__(self, vida):
+            self.vida = vida
+    
     def esta_vivo(self):
-        return self.vida != 0
-
+            return self.vida != 0
+    
     def recibir_disparo(self):
-        self.vida -= 1
+            self.vida -= 1
+
+class Soldado(Tropa):
+       def __init__(self):
+              super().__init__(1)
