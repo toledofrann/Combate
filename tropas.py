71c657720bb4e44ac98e46c7bd7df9a3d307fccf
toledofrann <toledofrancisco0303@gmail.com>
2026-09-21 19:34:21 -0300

    feat: implementa polimorfimo en el metodo disparar() para que cada clase haga su daño correspondiente (Fase Verde)

--- tropas.py
+++ tropas.py
@@ -7,20 +7,28 @@ class Tropa(ABC):
     def esta_vivo(self):
             return self.vida != 0
     
-    def recibir_disparo(self):
-            self.vida -= 1
+    def recibir_disparo(self, danio=1):
+            self.vida -= danio
 
+    @abstractmethod
     def disparar(self, objetivo):
-           objetivo.recibir_disparo()
+           pass
 
 class Soldado(Tropa):
        def __init__(self):
               super().__init__(1)
 
+       def disparar(self, objetivo):
+              objetivo.recibir_disparo(1)
+
 class Tanque(Tropa):
        def __init__(self):
               super().__init__(2)
+       def disparar(self, objetivo):
+              objetivo.recibir_disparo(2)
 
 class Buque(Tropa):
        def __init__(self):
               super().__init__(3)
+       def disparar(self, objetivo):
+              objetivo.recibir_disparo(3)
